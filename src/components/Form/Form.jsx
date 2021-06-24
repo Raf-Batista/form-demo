17230c893504d98d002eb98ed6a4368e7ab62f85
Raf-Batista <RafaelB2424@gmail.com>
2021-06-23 23:02:35 -0400

    Update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,36 +4,45 @@ import './Form.css';
 const Form = () => {
     return (
         <div className="form">
+            
                 <div className="form-container">
-                <div className="form-header">
-                  <h1>Sign up</h1>
-                </div>
+              
 
                 <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" />
-
-                    <label htmlFor="email">Username</label>
-                    <input type="email" name="email" />
-
-                    <label htmlFor="password">Username</label>
-                    <input type="password" name="password" />
-
-                    <div className="form-login-button">
-                        <button>Log in</button>
+                    <div className="form-header">
+                      <h1>Sign up</h1>
                     </div>
+              
+                    <label className="form-label-container-username">
+                        <input  type="text" name="username" placeholder="&nbsp;" autoComplete="off"  required />
+                        <span className="form-label-username">Username</span>
+                    </label>
+                    
+                    <label className="form-label-container-email">
+                        <input type="email" name="email" placeholder="&nbsp;" autoComplete="off"  required />
+                        <span className="form-label-email">Email</span>
+                    </label>
+                    
+                    <label className="form-label-container-password">
+                        <input type="password" name="password" placeholder="&nbsp;" autoComplete="off" required />
+                        <span className="form-label-password">Password</span>
+                    </label>
+                     
+                 
 
                     <div className="form-footer">
-                        <span>By signing up, you agree to our <a>Terms , Data Policy and Cookies Policy .</a></span>
+                        <span>By signing up, you agree to our <a href="#">Terms , Data Policy and Cookies Policy .</a></span>
                     </div>
+
                 </div>
 
                 <div className="form-signup">
-                    <span>Don't have an account? <a>Sign up</a></span>
+                    <span>Don't have an account? <a href="#">Sign up</a></span>
                 </div>
+
                 </div>
                 
-          
+             
         </div>
     )
 }
